65f57bf25888ef4097d8f900bfebb2fa9b3f3445
alaroche <alaroche@vertexinc.com>
2017-11-17 11:17:04 -0500

    Added radio-button.scss and variables.scss to radio-button folder to get the slider to work.

--- forms.module.ts
+++ forms.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {AuComponent} from './radio-button/au';
 import { DropdownComponent } from './dropdown/dropdown';
 import { RadioButtonComponent } from './radio-button/radio-button.component';
 import { YearSelectorComponent } from './year-selector/year-selector';
@@ -10,7 +10,8 @@ import { CommonModule } from "@angular/common";
   declarations: [
     DropdownComponent,
     RadioButtonComponent,
-    YearSelectorComponent
+    YearSelectorComponent,
+      AuComponent,
   ],
   imports:[
     CommonModule,
